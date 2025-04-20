7b84a9bf718ceb73129803b5e54c5a1f92e43af9
Deepika Javvaji <deepikachowdaryj@gmail.com>
2025-04-20 13:48:42 +0200

    data ingestion initiated

--- src/logger.py
+++ src/logger.py
@@ -16,5 +16,10 @@ logging.basicConfig(
     
 )
 
-
+if __name__ == "__main__":
+        try:
+            a = 1/0
+        except Exception as e:
+            logging.info("Divide by zero")
+            raise CustomException(e, sys) 
 
